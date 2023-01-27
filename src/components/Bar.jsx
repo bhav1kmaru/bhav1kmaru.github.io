19b495472e96e78de0938e77de4d1ff08dc46515
bhav1kmaru <bhavik.m01@gmail.com>
2023-01-27 17:42:43 +0530

    bugfix

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -131,7 +131,7 @@ export default function Bar() {
             // isActive={index === 2}
             isActive={currentPage == item.toLowerCase()}
           >
-            <Link
+            {/* <Link
               color="inherit"
               css={{
                 minWidth: "100%",
@@ -140,7 +140,8 @@ export default function Bar() {
               onClick={() => setCurrentPage(item.toLowerCase())}
             >
               {item}
-            </Link>
+            </Link> */}
+            <Text>{item}</Text>
           </Navbar.CollapseItem>
         ))}
         <Navbar.CollapseItem>
