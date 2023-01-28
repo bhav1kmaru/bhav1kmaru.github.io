03199826007621a2f7754a070432b46c1a9acefa
bhav1kmaru <bhavik.m01@gmail.com>
2023-01-28 18:03:30 +0530

    added border to form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
-import { Button, Input, Textarea } from "@nextui-org/react";
+import { Button, Input, Textarea,css, useTheme } from "@nextui-org/react";
 export default function ContactForm() {
   const [state, handleSubmit] = useForm("mjvdgypr");
+  const {theme}=useTheme()
   if (state.succeeded) {
     return <h3 style={{marginTop:"20px"}}>Thanks for Contacting Me!</h3>;
   }
@@ -10,12 +11,23 @@ export default function ContactForm() {
     <div style={{ marginTop: "50px" }}>
       <form onSubmit={handleSubmit}>
         <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
-          <Input labelPlaceholder="Name" name="name" />
+          <Input
+            css={{
+              border: "1px solid",
+              borderColor: theme.colors.neutral.value,
+            }}
+            labelPlaceholder="Name"
+            name="name"
+          />
           <Input
             labelPlaceholder="Email"
             id="email"
             type="email"
             name="email"
+            css={{
+              border: "1px solid",
+              borderColor: theme.colors.neutral.value,
+            }}
           />
           <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
@@ -25,6 +37,10 @@ export default function ContactForm() {
             id="message"
             labelPlaceholder="Message"
             name="message"
+            css={{
+              border: "1px solid",
+              borderColor: theme.colors.neutral.value,
+            }}
           />
           <ValidationError
             prefix="Message"
@@ -32,8 +48,19 @@ export default function ContactForm() {
             errors={state.errors}
           />
         </div>
-        <div style={{display:"flex",justifyContent:"center",marginTop:"20px"}}>
-          <Button ghost color='gradient' type="submit" disabled={state.submitting}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          <Button
+            ghost
+            color="gradient"
+            type="submit"
+            disabled={state.submitting}
+          >
             Submit
           </Button>
         </div>
