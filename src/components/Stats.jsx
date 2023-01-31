898a37d98905cb2f883c5ea4efe29610690b91cb
bhav1kmaru <bhavik.m01@gmail.com>
2023-01-31 16:36:26 +0530

    added more emojis

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -37,7 +37,23 @@ const Stats = () => {
         whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
         transition={{ duration: 1 }}
       >
-        <h2 style={{ marginTop: "50px" }}>My GitHub Stats</h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            marginTop: "50px",
+            gap: "10px",
+          }}
+        >
+          {" "}
+          <h2>GitHub Stats</h2>
+          <img
+            src="https://em-content.zobj.net/source/microsoft-teams/337/star_2b50.png"
+            style={{ height: "45px" }}
+          />
+        </div>
         <img
           src={`https://github-readme-stats-bhav1kmaru.vercel.app/api?username=bhav1kmaru&hide=prs&count_private=true&show_icons=true&theme=${statTheme}`}
         />
@@ -46,7 +62,23 @@ const Stats = () => {
         whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
         transition={{ duration: 1 }}
       >
-        <h2 style={{ marginTop: "50px" }}>Top Languages</h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            marginTop: "50px",
+            gap: "10px",
+          }}
+        >
+          {" "}
+          <h2>Top Languages</h2>
+          <img
+            src="https://em-content.zobj.net/source/microsoft-teams/337/bar-chart_1f4ca.png"
+            style={{ height: "45px" }}
+          />
+        </div>
         <img
           src={`https://github-readme-stats-bhav1kmaru.vercel.app/api/top-langs/?username=bhav1kmaru&hide=prs&count_private=true&show_icons=true&theme=${statTheme}`}
         />
@@ -55,7 +87,23 @@ const Stats = () => {
         whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
         transition={{ duration: 1 }}
       >
-        <h2 style={{ marginTop: "50px" }}>Current Streak</h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            marginTop: "50px",
+            gap: "10px",
+          }}
+        >
+          {" "}
+          <h2>Current Streak</h2>
+          <img
+            src="https://em-content.zobj.net/source/microsoft-teams/337/fire_1f525.png"
+            style={{ height: "45px" }}
+          />
+        </div>
         <img
           src={`https://github-readme-streak-stats.herokuapp.com?user=bhav1kmaru&theme=${statTheme}`}
           alt="bhav1kmaru"
@@ -65,22 +113,36 @@ const Stats = () => {
         whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
         transition={{ duration: 1 }}
       >
-        <h2 style={{ marginTop: "50px" }}>GitHub Calendar</h2>
-        <div className='calendar'>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            marginTop: "50px",
+            gap: "10px",
+          }}
+        >
+          {" "}
+          <h2>GitHub Calendar</h2>
+          <img
+            src="https://em-content.zobj.net/source/microsoft-teams/337/spiral-calendar_1f5d3-fe0f.png"
+            style={{ height: "45px" }}
+          />
+        </div>
+        <div className="calendar">
           <GitHubCalendar
             username="bhav1kmaru"
             style={{ margin: "auto" }}
             blockSize={12}
             fontSize={15}
             blockMargin={8}
-            
           >
             {" "}
             <div>
               <ReactTooltip delayShow={20} html />
             </div>
           </GitHubCalendar>
-          
         </div>
       </motion.div>
     </motion.div>
